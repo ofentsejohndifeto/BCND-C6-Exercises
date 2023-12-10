8181b37e910ec0f62b67ed44f34e6cb3448f736a
ofentsejohndifeto <ofentsejohnd22@gmail.com>
2023-12-10 18:47:57 +0200

    exeercise C6A works

--- contracts/ExerciseC6A.sol
+++ contracts/ExerciseC6A.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.25;
+pragma solidity ^0.8.19;
 
 contract ExerciseC6A {
 
@@ -6,7 +6,7 @@ contract ExerciseC6A {
     /*                                       DATA VARIABLES                                     */
     /********************************************************************************************/
 
-
+    uint constant M = 2;
     struct UserProfile {
         bool isRegistered;
         bool isAdmin;
@@ -15,8 +15,10 @@ contract ExerciseC6A {
     address private contractOwner;                  // Account used to deploy contract
     mapping(address => UserProfile) userProfiles;   // Mapping for storing user profiles
 
+    bool private operational = true;                                    
 
-
+    address[] multiCalls = new address[](0); // track all addresses that have called fucniton that needs to implement multi-partyy concesscus
+    
     /********************************************************************************************/
     /*                                       EVENT DEFINITIONS                                  */
     /********************************************************************************************/
@@ -51,6 +53,17 @@ contract ExerciseC6A {
         _;
     }
 
+    /**
+    * @dev Modifier that requires the "operational" boolean variable to be "true"
+    *      This is used on all state changing functions to pause the contract in 
+    *      the event there is an issue that needs to be fixed
+    */
+    modifier requireIsOperational() 
+    {
+        require(operational, "Contract is currently not operational");
+        _;  // All modifiers require an "_" which indicates where the function body will be added
+    }
+
     /********************************************************************************************/
     /*                                       UTILITY FUNCTIONS                                  */
     /********************************************************************************************/
@@ -72,6 +85,20 @@ contract ExerciseC6A {
         return userProfiles[account].isRegistered;
     }
 
+    /**
+    * @dev Get operating status of contract
+    *
+    * @return A bool that is the current operating status
+    */      
+    function isOperational() 
+                            public 
+                            view 
+                            returns(bool) 
+    {
+        return operational;
+    }
+
+
     /********************************************************************************************/
     /*                                     SMART CONTRACT FUNCTIONS                             */
     /********************************************************************************************/
@@ -83,6 +110,7 @@ contract ExerciseC6A {
                                 )
                                 external
                                 requireContractOwner
+                                requireIsOperational
     {
         require(!userProfiles[account].isRegistered, "User is already registered.");
 
@@ -91,5 +119,36 @@ contract ExerciseC6A {
                                                 isAdmin: isAdmin
                                             });
     }
-}
+
+    /**
+    * @dev Sets contract operations on/off
+    *
+    * When operational mode is disabled, all write transactions except for this one will fail
+    */    
+    function setOperatingStatus
+                            (
+                                bool mode
+                            ) 
+                            external
+    {
+        require(mode != operational, "New mode must be different from existing mode");
+        require(userProfiles[msg.sender].isAdmin, "Caller is not an admin");
+
+        bool isDuplicate = false;
+        for(uint c=0; c<multiCalls.length; c++) {
+            if (multiCalls[c] == msg.sender) {  //loop through values of array of addresses that haev set teh fucniton operational to make sure they cant do it again
+                isDuplicate = true;
+                break;
+            }
+        }
+        require(!isDuplicate, "Caller has already called this function.");
+
+        multiCalls.push(msg.sender); //checks multi-party call, changes mode
+        if (multiCalls.length >= M) {
+            operational = mode;      
+            multiCalls = new address[](0);      // reintialises funciton calls, otherwise defeats multicall concensus
+        }
+    }
+
+
 
